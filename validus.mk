5645e5de13f170ea85efe61797597cf750292d27
hardwareadictos <hardwareadictos@gmail.com>
2016-12-23 13:30:01 +0100

    fixed QcomOMXMetadata.h build fail

--- validus.mk
+++ validus.mk
@@ -1,16 +1,16 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 768
+TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/mako/full_mako.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mako
-PRODUCT_NAME := cm_mako
+PRODUCT_NAME := validus_mako
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
